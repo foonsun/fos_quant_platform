bbdf925047ea43ff7e8a85a3419b0c338ef9830a
foonsun <andyjoe318@gmail.com>
2019-03-31 19:18:57 +0800

    * add oscar account

--- blockapps/oscar/apps/checkout/views.py
+++ blockapps/oscar/apps/checkout/views.py
@@ -11,6 +11,12 @@ from django.views import generic
 
 from oscar.core.loading import get_class, get_classes, get_model
 
+from oscar_accounts import exceptions as act_exceptions
+from oscar_accounts import security
+from oscar_accounts.checkout import forms, gateway
+from oscar_accounts.checkout.allocation import Allocations
+
+
 from . import signals
 
 ShippingAddressForm, ShippingMethodForm, GatewayForm \
@@ -23,6 +29,8 @@ RedirectRequired, UnableToTakePayment, PaymentError \
     = get_classes('payment.exceptions', ['RedirectRequired',
                                          'UnableToTakePayment',
                                          'PaymentError'])
+Source = get_model('payment', 'Source')
+SourceType = get_model('payment', 'SourceType')
 UnableToPlaceOrder = get_class('order.exceptions', 'UnableToPlaceOrder')
 OrderPlacementMixin = get_class('checkout.mixins', 'OrderPlacementMixin')
 CheckoutSessionMixin = get_class('checkout.session', 'CheckoutSessionMixin')
@@ -719,21 +727,56 @@ class MultiPaymentDetailsView(RedirectSessionMixin, PaymentDetailsView):
     paymentsource_name={
         'alipay_warrant':"支付宝担保",
         'weixin_payjs': "微信支付",
+        'oscar_account': "账户支付",
         }
     paymentsource_method={
         'alipay_warrant':AlipayHandle,
         'weixin_payjs': PayjsHandle,
+        #'oscar_account': 
     }
     def get_context_data(self, **kwargs):
-        context=super(PaymentDetailsView,self).get_context_data(**kwargs)
-        context['paymethod']=self.paymentsource_name[self.get_paymethod()]
-        return context
+        ctx=super(PaymentDetailsView,self).get_context_data(**kwargs)
+        ctx['paysource'] = self.get_paymethod()
+        ctx['paymethod']=self.paymentsource_name[self.get_paymethod()]
+        if self.get_paymethod() == 'oscar_account':
+            # Add variable to indicate if the user is blocked from paying with
+            # accounts.
+            ctx['is_blocked'] = security.is_blocked(self.request)
+
+            form = forms.ValidAccountForm(self.request.user)
+            ctx['account_form'] = form
+
+            # Add accounts that are linked to this user
+            if self.request.user.is_authenticated():
+                ctx['user_accounts'] = gateway.user_accounts(self.request.user)
+
+            # Add existing allocations to context
+            allocations = self.get_account_allocations()
+            ctx['account_allocations'] = allocations
+            order_total = ctx['order_total']
+            total_for_allocation = order_total.incl_tax if order_total.is_tax_known else order_total.excl_tax
+            ctx['to_allocate'] = total_for_allocation - allocations.total
+
+        return ctx
 
     def get(self, request, *args, **kwargs):
         if kwargs.get('paymethod'):
             self.save_paymethod(kwargs.get('paymethod'))
             return HttpResponseRedirect('/checkout/preview')
         return super(PaymentDetailsView,self).get(self,request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        # Intercept POST requests to look for attempts to allocate to an
+        # account, or remove an allocation.
+        action = self.request.POST.get('action', None)
+        if action == 'select_account':
+            return self.select_account(request)
+        elif action == 'allocate':
+            return self.add_allocation(request)
+        elif action == 'remove_allocation':
+            return self.remove_allocation(request)
+        return super(MultiPaymentDetailsView,self).post(request, *args, **kwargs)
+
     def handle_payment(self, order_number, order_total, **kwargs):
         '''
         处理支付请求
@@ -742,7 +785,125 @@ class MultiPaymentDetailsView(RedirectSessionMixin, PaymentDetailsView):
         :param kwargs:
         :return:
         '''
-        self.set_order_number(order_number)
-        self.set_info()
-        paymethod=self.paymentsource_method[self.get_paymethod()]
-        paymethod(self,order_number, **kwargs)
+        if self.get_paymethod() != 'oscar_account':
+            self.set_order_number(order_number)
+            self.set_info()
+            paymethod=self.paymentsource_method[self.get_paymethod()]
+            paymethod(self,order_number, **kwargs)
+        else:
+            # Override payment method to use accounts to pay for the order
+            allocations = self.get_account_allocations()
+            if allocations.total != order_total.incl_tax:
+                raise UnableToTakePayment(
+                    "Your account allocations do not cover the order total")
+
+            try:
+                gateway.redeem(order_number, self.request.user, allocations)
+            except act_exceptions.AccountException:
+                raise UnableToTakePayment(
+                    "An error occurred with the account redemption")
+
+            # If we get here, payment was successful.  We record the payment
+            # sources and event to complete the audit trail for this order
+            source_type, __ = SourceType.objects.get_or_create(
+                name="Account")
+            for code, amount in allocations.items():
+                source = Source(
+                    source_type=source_type,
+                    amount_debited=amount, reference=code)
+                self.add_payment_source(source)
+            self.add_payment_event("Settle", order_total.incl_tax)
+        
+        # Custom form-handling methods
+
+    def select_account(self, request):
+        ctx = self.get_context_data()
+
+        # Check for blocked users
+        if security.is_blocked(request):
+            messages.error(request,
+                           "You are currently blocked from using accounts")
+            return http.HttpResponseRedirect(
+                reverse('checkout:payment-details'))
+
+        # If account form has been submitted, validate it and show the
+        # allocation form if the account has non-zero balance
+        form = forms.ValidAccountForm(self.request.user,
+                                      self.request.POST)
+        ctx['account_form'] = form
+        if not form.is_valid():
+            security.record_failed_request(self.request)
+            return self.render_to_response(ctx)
+
+        security.record_successful_request(self.request)
+        ctx['allocation_form'] = forms.AllocationForm(
+            form.account, self.request.basket,
+            ctx['shipping_charge'].incl_tax if ctx['shipping_charge'].is_tax_known else ctx['shipping_charge'].excl_tax,
+            ctx['order_total'].incl_tax if ctx['order_total'].is_tax_known else ctx['order_total'].excl_tax,
+            self.get_account_allocations())
+        return self.render_to_response(ctx)
+
+    def add_allocation(self, request):
+        # We have two forms to validate, first check the account form
+        account_form = forms.ValidAccountForm(request.user,
+                                              self.request.POST)
+        if not account_form.is_valid():
+            # Only manipulation can get us here
+            messages.error(request,
+                           _("An error occurred allocating from your account"))
+            return http.HttpResponseRedirect(reverse(
+                'checkout:payment-details'))
+
+        # Account is still valid, now check requested allocation
+        ctx = self.get_context_data()
+        allocation_form = forms.AllocationForm(
+            account_form.account, self.request.basket,
+            ctx['shipping_charge'].incl_tax if ctx['shipping_charge'].is_tax_known else ctx['shipping_charge'].excl_tax,
+            ctx['order_total'].incl_tax if ctx['order_total'].is_tax_known else ctx['order_total'].excl_tax,
+            self.get_account_allocations(),
+            data=self.request.POST)
+        if not allocation_form.is_valid():
+            ctx = self.get_context_data()
+            ctx['allocation_form'] = allocation_form
+            ctx['account_form'] = account_form
+            return self.render_to_response(ctx)
+
+        # Allocation is valid - record in session and reload page
+        self.store_allocation_in_session(allocation_form)
+        messages.success(request, _("Allocation recorded"))
+        return http.HttpResponseRedirect(reverse(
+            'checkout:payment-details'))
+
+    def remove_allocation(self, request):
+        code = None
+        for key in request.POST.keys():
+            if key.startswith('remove_'):
+                code = key.replace('remove_', '')
+        allocations = self.get_account_allocations()
+        if not allocations.contains(code):
+            messages.error(
+                request, _("No allocation found with code '%s'") % code)
+        else:
+            allocations.remove(code)
+            self.set_account_allocations(allocations)
+            messages.success(request, _("Allocation removed"))
+        return http.HttpResponseRedirect(reverse('checkout:payment-details'))
+
+    def store_allocation_in_session(self, form):
+        allocations = self.get_account_allocations()
+        allocations.add(form.account.code, form.cleaned_data['amount'])
+        self.set_account_allocations(allocations)
+
+    # The below methods could be put onto a customised version of
+    # oscar.apps.checkout.utils.CheckoutSessionData.  They are kept here for
+    # simplicity
+
+    def get_account_allocations(self):
+        allocation_data = self.checkout_session._get('accounts', 'allocations', '{}')
+        return Allocations.deserialize(allocation_data)
+
+    def set_account_allocations(self, allocations):
+        return self.checkout_session._set('accounts', 'allocations', Allocations.serialize(allocations))
+
+
+        

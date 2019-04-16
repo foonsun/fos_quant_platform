c7d845381794845ef7da40a64ab6237d560a7d14
foonsun <andyjoe318@gmail.com>
2019-04-16 19:55:47 +0800

    * add limitbuy script

--- blockapps/limitbuy/management/commands/limitbuy.py
+++ blockapps/limitbuy/management/commands/limitbuy.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
-from duiqiao.business import DuiQiao
+import time
+from limitbuy.business import LimitBuy
 import logging
+from django.utils import timezone
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -18,6 +20,8 @@ class Command(BaseCommand):
                 help='min sell price')
         parser.add_argument('--v', type=float, dest='base_volume', required=True, \
                 help='basevolume to use')
+        parser.add_argument('--p', type=float, dest='limit_price', required=True, \
+                help='limit price to use')
 
     def handle(self, *args, **options):
         exchange = options['exchange']
@@ -27,5 +31,12 @@ class Command(BaseCommand):
         base_volume = options['base_volume']
         max_buy_price = options['max_buy_price']
         min_sell_price = options['min_sell_price']
-        policy = DuiQiao(exchange, symbol, publickey, privatekey, max_buy_price, min_sell_price, base_volume)
+        limit_price = options['limit_price']
+        start_time = 0
+        nowtime = time.time()
+        while True:
+            if int(nowtime*1000) > start_time - 20:
+                break
+            time.sleep(0.05)
+        policy = LimitBuy(exchange, symbol, publickey, privatekey, max_buy_price, min_sell_price, base_volume, limit_price)
         policy.run()
